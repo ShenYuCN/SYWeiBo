05fbfd7fbe89f69be055fef4a3de279d6bb5bab9
ShenYu <shenyu_sy@outlook.com>
2015-10-09 17:15:43 +0800

    Initial commit

---
+++ SYWeiBo/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYWeiBo
+//
+//  Created by Shen Yu on 15/10/9.
+//  Copyright © 2015年 Shen Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
