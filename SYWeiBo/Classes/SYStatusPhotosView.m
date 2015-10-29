e443fa096dd02b6cccf49995e387491ed0a1408a
ShenYuCN <shenyu_sy@outlook.com>
2015-10-29 13:02:53 +0800

    修改配图显示1张图的大小 1张图 4张图 9张图

--- SYWeiBo/Classes/SYStatusPhotosView.m
+++ SYWeiBo/Classes/SYStatusPhotosView.m
@@ -13,15 +13,18 @@
 #import "UIView+Extension.h"
 
 
+
 //每张配图的宽高
 #define kSYStatusPhotosWH  (([UIScreen mainScreen].bounds.size.width - 40) / 3)
 //每张配图的间距
 #define kSYStatusPhotosMargin 10
 //最大列数
 #define kSYPhotoViewMaxCol(count) ((count == 4)?2:3)
+@interface SYStatusPhotosView()
+@property (nonatomic,assign) CGFloat photosWH;
+@end
 
 @implementation SYStatusPhotosView
-
 -(instancetype)initWithFrame:(CGRect)frame{
     if (self = [super initWithFrame:frame]) {
     }
@@ -52,20 +55,29 @@
 -(void)layoutSubviews{
     [super layoutSubviews];
     
-    int maxCol = kSYPhotoViewMaxCol(self.photos.count);
+    int maxCol = [self photosColCount:self.photos.count];
  
     for (int i = 0; i < self.photos.count; i ++) {
         SYStatusPhotoView *photoView = self.subviews[i];
         int col = i % maxCol;
         int row = i / maxCol;
-        CGFloat photoX = col * (kSYStatusPhotosWH + kSYStatusPhotosMargin);
-        CGFloat photoY = row * (kSYStatusPhotosWH + kSYStatusPhotosMargin);
-        CGFloat photoW = kSYStatusPhotosWH;
-        CGFloat photoH = kSYStatusPhotosWH;
+        
+        CGFloat photoW = self.photosWH;
+        CGFloat photoH = photoW;
+        CGFloat photoX = col * (photoW + kSYStatusPhotosMargin);
+        CGFloat photoY = row * (photoW + kSYStatusPhotosMargin);
         photoView.frame = CGRectMake(photoX, photoY, photoW, photoH);
     }
 }
 
+-(int)photosColCount:(NSUInteger)photosCount{
+    if (photosCount == 1){
+        self.photosWH = ([UIScreen mainScreen].bounds.size.width - 30) / 2;
+        return 1;
+    }
+    self.photosWH = kSYStatusPhotosWH;
+    return (photosCount == 4) ? 2 : 3;
+}
 /**
  *  配图的整体大小
  *
@@ -75,15 +87,19 @@
     
     //最大的列数
     int maxCols = 3;
+    CGFloat photoWH = (([UIScreen mainScreen].bounds.size.width - 40) / 3);
+    if (count == 1){
+        photoWH = ([UIScreen mainScreen].bounds.size.width - 30) / 2;
+    }
     
     //列数
     int cols = (count >= maxCols) ? maxCols : count;
-    CGFloat photoW = cols * kSYStatusPhotosWH + (cols -1) * kSYStatusPhotosMargin;
+    CGFloat photoW = cols * photoWH + (cols -1) * kSYStatusPhotosMargin;
     
     
     //行数
     int rows = (count + maxCols - 1) / maxCols;
-    CGFloat photoH = rows * kSYStatusPhotosWH + (rows -1) * kSYStatusPhotosMargin;
+    CGFloat photoH = rows * photoWH + (rows -1) * kSYStatusPhotosMargin;
     
     return CGSizeMake(photoW, photoH);
     
